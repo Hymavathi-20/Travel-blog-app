634b60c75fcf79c54117b7e599615d2a6687b561
Hymavathi-20 <mhymavathi5920@gmail.com>
2026-05-29 15:30:46 +0530

    Add comments and dotenv

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+// Import required modules
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import dotenv from "dotenv";
 
-// e62062d8596f51485690638ea805d536
+// Configure environment variables
+dotenv.config();
+
+// Initialize express app and API key
 const app=express();
 const port=3000;
-const API="e62062d8596f51485690638ea805d536";
+const API=process.env.API_KEY;
 
+// Store sample travel blogs temporarily (without database)
 const travelBlogs = [
   {
     name: "Exploring the Streets of Paris",
@@ -35,25 +41,31 @@ const travelBlogs = [
   }
 ];
 
-app.use(bodyParser.urlencoded({extended:true}));
+// Middleware to access static files and form data
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
+// Render home page with all blogs
 app.get("/",(req,res)=>{
     res.render("index.ejs",{blogs :travelBlogs});
 });
 
+// Render form page to create a new blog
 app.get("/create",(req,res)=>{
     res.render("create.ejs");
 });
 
+// Render form page to delete a blog
 app.get("/delete",(req,res)=>{
     res.render("delete.ejs");
 });
 
+// Render form page to edit a blog
 app.get("/edit",(req,res)=>{
     res.render("edit.ejs");
 });
 
+// Add newly created blog to array
 app.post("/createBlog",(req,res)=>{
     const newdata={name: req.body["name"], description : req.body["description"]};
     travelBlogs.push(newdata);
@@ -61,6 +73,7 @@ app.post("/createBlog",(req,res)=>{
     res.redirect("/");
 });
 
+// Delete blog using blog name
 function deleteBlog(bname){
     const idx= travelBlogs.findIndex(
         blog=> blog.name.toLowerCase()=== bname.toLowerCase()
@@ -71,6 +84,7 @@ function deleteBlog(bname){
     }
 }
 
+// Remove blog after form submission
 app.post("/deleteblog",(req,res)=>{
     const blogName=req.body["name"];
     deleteBlog(blogName);
@@ -78,6 +92,7 @@ app.post("/deleteblog",(req,res)=>{
     res.redirect("/");
 });
 
+// Edit blog description using blog name
 function editBlog(bname,newdes){
     const blog= travelBlogs.find(
         blog => blog.name.toLowerCase()===bname.toLowerCase()
@@ -87,6 +102,7 @@ function editBlog(bname,newdes){
     }
 }
 
+// Update blog after edit form submission
 app.post("/editBlog",(req,res)=>{
     const blogName=req.body["name"];
     const newDes=req.body["description"];
@@ -95,7 +111,7 @@ app.post("/editBlog",(req,res)=>{
     res.redirect("/");
 });
 
-
+// Generate current date details for weather card
 const getdate= ()=>{
     const months=["Jan","Feb","March","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
     const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
@@ -111,17 +127,22 @@ const getdate= ()=>{
     };
 };
 
+// Render weather search page
 app.get("/getWeather",(req,res)=>{
     res.render("weather.ejs");
 })
 
+// Fetch weather data from OpenWeatherMap API
 app.post("/getWeather", async(req,res)=>{
-    
     try{
+        // Get place entered by user
         const place=req.body.place;
+
+        // Send request to OpenWeatherMap API
         const response=await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${API}&units=metric`);
         const result=response.data;
-        //const imgURL=`https://openweathermap.org/img/wn/${icon}@2x.png`
+
+        // Extract only required weather details
         const data={
             img: `https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png` ,
             temp:result.main.temp,
@@ -132,11 +153,12 @@ app.post("/getWeather", async(req,res)=>{
             con:result.sys.country,
             date: getdate()
         };
-        //console.log(data);
+        // Render weather report on frontend
         res.render("weather.ejs",{weather:data});
-    }catch(error){
-        console.log(error);
-        console.log(error.response.data.message)
+    }// Handle invalid city or API errors
+    catch(error){
+        // console.log(error);
+        // Display error message to user
         res.render("weather.ejs",{errormsg :error.response.data.message });
     }
     /*
@@ -156,7 +178,7 @@ app.post("/getWeather", async(req,res)=>{
     */
 });
 
-
+// Start server
 app.listen(port,(req,res)=>{
     console.log(`Server is rumming on port ${port}`);
 });
